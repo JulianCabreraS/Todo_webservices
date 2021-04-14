8f8b2c62fd46acea2b09238ef8b0d1c8065cf13d
jcabrera <julian.cabreras@gmail.com>
2021-04-13 20:29:36 -0500

    3. Implementation of basic AuthenticationBean and BasicAuthenticationController.

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/auth/AuthenticationBean.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic.auth;
+
+public class AuthenticationBean {
+
+
+    private final String message;
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,9 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -17,6 +20,7 @@ public class TodoResource {
         return todoService.findAll();
     }
 
+    //* GET /users/{username}/todos/{id}
     @GetMapping(path ="/users/{username}/todos/{id}")
     public Todo getTodo(@PathVariable String username, @PathVariable long id){
         return todoService.findbyId(id);
@@ -30,5 +34,22 @@ public class TodoResource {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+    //* PUT /users/{username}/todos/{id}
+    @PutMapping(path ="/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+        Todo todoUpdated= todoService.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    //* POST /users/{user_name}/todos
+    @PostMapping(path ="/users/{username}/todos")
+    public ResponseEntity<Void> updateTodo(@PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.save(todo);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+
     }
 }
